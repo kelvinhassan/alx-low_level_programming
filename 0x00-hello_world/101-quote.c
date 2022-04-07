d07c5eb99492101ac183abb910110028956b582f
kelvinhassan <ngerajustin@gmail.com>
2022-04-07 12:33:34 -0700

    return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -program should compile without warning
+ * Return: 1
+ */
+int main(void)
+{
+
+fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+return (1);
+
+}
