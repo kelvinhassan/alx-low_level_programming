a080cefb9ff3453daf55be2a1c1472047f4223e6
kelvinhassan <ngerajustin@gmail.com>
2022-04-07 10:47:48 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#innclude <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ * main - main block -printd out a string to stdout
+ * return: 0 if success.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
