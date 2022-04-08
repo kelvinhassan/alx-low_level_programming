583f495d2ecd85751cc91e62ab9c1c9c4ce1d9be
kelvinhassan <ngerajustin@gmail.com>
2022-04-08 14:21:12 -0700

    char - e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets except q and c
+ * Return: 0 in success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
