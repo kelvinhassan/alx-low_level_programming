41f058a6d6b746c937da5d233f219b66edb4b905
kelvinhassan <ngerajustin@gmail.com>
2022-04-09 02:05:17 -0700

    fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,9 @@
  */
 int main(void)
 {
-	int i = 0;
-	int j = 0;
-	int k = 0;
+	int i = '0';
+	int j = '0';
+	int k = '0';
 
 	while (i <= '7')
 	{
@@ -35,7 +35,7 @@ int main(void)
 			k = '0';
 			j++;
 		}
-		j='0';
+		j ='0';
 		i++;
 	}
 	putchar('\n');
