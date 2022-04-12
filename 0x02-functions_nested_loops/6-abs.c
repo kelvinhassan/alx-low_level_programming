a47cf8fc96b1f9a5262a5a9fdc179a05aa1c0325
kelvinhassan <ngerajustin@gmail.com>
2022-04-12 10:34:04 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "maih.h"
+/**
+ * _abs - computer the absolute value of numbers from zero
+ * @i: integer to be converted
+ * Return: absolute value of input int
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * -1;
+	}
+	return (i);
+}
