2c8142031f2703ce44f36824110b7560a463eada
kelvinhassan <ngerajustin@gmail.com>
2022-04-12 10:07:57 -0700

    english alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - chceks whether character is from English alphabet
+ * @c: character to be checked
+ * Return: 1 if character is English
+ * 0 if not
+ */
+int _isalpha(intc)
+{
+	if((c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
