4e4bd007cf6c990f5aac42fe8083b51d4f35ab41
kelvinhassan <ngerajustin@gmail.com>
2022-04-20 12:53:57 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,15 @@
  * @s: pointer to the string to check
  * Return: void
  */
+
 int _strlen(char *s)
 {
-	int i = 0;
+	int l = 0;
 
-	while (*s != '\0')
+	int l = 0;
 	{
 		s++;
-		i++;
+		l++;
 	}
-	return (1);
+	return (l);
 }
