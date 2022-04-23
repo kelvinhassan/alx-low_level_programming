496ed01767aa381fa29598c787f817660c4a6e93
kelvinhassan <ngerajustin@gmail.com>
2022-04-23 03:29:03 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * print_number - function that prints an integer
+ * You can only use _putchar function to print
+ * You are not allowed to use long
+ * You are not allowed to use arrays or pointers
+ * You are not allowed to hard-code special values
+ * Return: void
+ */
+void print_number(int n)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	 return (0);
+}
