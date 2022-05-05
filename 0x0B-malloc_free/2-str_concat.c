a45a225b7d32a0a5008242655c27293143989ecc
kelvinhassan <ngerajustin@gmail.com>
2022-05-05 11:12:47 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,12 @@ char *str_concat(char *s1, char *s2)
 	int n1;
 	int n2;
 	int count;
+
 	count = 0;
 	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (n1 = 0; s1[n1] != '\0'; n1++)
 		;
 	for (n2 = 0; s2[n2] != '\0'; n2++)
