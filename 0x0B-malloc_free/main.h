e911dd05680f567a4d5019e651844d2b908919da
kelvinhassan <ngerajustin@gmail.com>
2022-05-05 11:31:48 -0700

    fix

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,11 +5,17 @@
 #include <stdlib.h>
 
 char *_strdup(char *str);
+
 char **strtow(char *str);
-char *argstostr(int ac,char **av);
-char *str_concat(char *s1, char s2);
+
+char *argstostr(int ac, char **av);
+
+char *str_concat(char *s1, char *s2);
+
 void free_grid(int **grid, int height);
+
 int **alloc_grid(int width, int height);
+
 char *create_array(unsigned int size, char c);
 
 #endif
