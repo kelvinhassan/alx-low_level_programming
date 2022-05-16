e5fce9fde23e441b39e3f1ebfd029133a0d8ff3e
kelvinhassan <ngerajustin@gmail.com>
2022-05-16 13:48:42 -0700

    task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,23 +2,41 @@
 #include <stdlib.h>
 
 /**
- * main - searchs for an integer
+ * main - prints its own opcodes
  * @argc: number of arguments
  * @argv: array with arguments
- * Return: no return
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
+	int bytes, i;
+	char *arr;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (atoi(argv[1]) < 0)
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
